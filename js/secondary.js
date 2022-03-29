29534f85d5fcb5f9a68bea897189229b6344161f
gkasianenko <g.kasianenko@yandex.ru>
2022-03-29 17:59:36 +0300

    product card img click

--- js/secondary.js
+++ js/secondary.js
@@ -2,6 +2,9 @@ const uploadInput = document.querySelector("#input-upload");
 
 const uploadButton = document.querySelector(".contacts-form__file");
 
+const imgDiv = document.querySelector('.product__main-photo');
+
+imgDiv.addEventListener("click", changeImg)
 
 uploadButton.addEventListener("click", initUpload);
 
@@ -9,3 +12,33 @@ function initUpload(){
     uploadInput.click();
 }
 
+function changeImg() {
+    const image = document.querySelector('.main-photo');
+
+    if (image.src.match("./img/product/main.jpg")) {
+        image.src = "./img/product/sub-1.jpg";
+        return;
+    }
+
+    if (image.src.match("./img/product/sub-1.jpg")){
+        image.src = "./img/product/sub-2.jpg";
+        return;
+    }
+    
+    if(image.src.match("./img/product/sub-2.jpg")){
+        image.src = "./img/product/sub-3.jpg";
+        return;
+    }
+
+    if(image.src.match("./img/product/sub-3.jpg")){
+        image.src = "./img/product/sub-4.jpg";
+        return;
+    }
+
+    if(image.src.match("./img/product/sub-4.jpg")){
+        image.src = "./img/product/main.jpg";
+        return;
+    }
+
+
+}
