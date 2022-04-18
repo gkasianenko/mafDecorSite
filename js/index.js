e8697ee3a15b19688820aa885fa9dba70b294117
gkasianenko <g.kasianenko@yandex.ru>
2022-04-19 02:43:51 +0300

    main page adaptive + mobmenu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-window.addEventListener("scroll", changeNavBar);
+const mobMenuButton = document.querySelector(".header__burger-menu");
+const mobMenu = document.querySelector(".mobile-menu");
+const mobMenuClose = document.querySelector(".mobile-menu__close");
 
-function changeNavBar(){
-    let header = document.querySelector("header");
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('header-scroll-active', windowPosition);
+window.addEventListener("scroll", changeNavBar);
+mobMenuButton.addEventListener("click", openMobMenu);
+mobMenuClose.addEventListener("click", closeMobMenu);
+
+function changeNavBar() {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("header-scroll-active", windowPosition);
+}
+
+function openMobMenu() {
+  mobMenu.classList.add("active");
+}
+
+function closeMobMenu() {
+  mobMenu.classList.remove("active");
 }
