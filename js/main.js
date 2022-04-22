6642970d88f5324b0e3f43371a729e4fa8495ae2
gkasianenko <g.kasianenko@yandex.ru>
2022-04-23 01:39:56 +0300

    ledders page 50% adaptive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import categoryPage from "./pages/category-ledders.js";
 
-
 const state = {};
 
 //для теста, потом стереть
@@ -13,18 +12,33 @@ const uploadInput = document.querySelector("#input-upload");
 
 const uploadButton = document.querySelector(".contacts-form__file");
 
+//Mobmenu vars
+const mobMenuButton = document.querySelector(".header__burger-menu");
+const mobMenu = document.querySelector(".mobile-menu");
+const mobMenuClose = document.querySelector(".mobile-menu__close");
+
+//Mobmenu funcs
+mobMenuButton.addEventListener("click", openMobMenu);
+mobMenuClose.addEventListener("click", closeMobMenu);
 
 uploadButton.addEventListener("click", initUpload);
 
-function initUpload(){
-    uploadInput.click();
+function initUpload() {
+  uploadInput.click();
 }
-
 
 window.addEventListener("scroll", changeNavBar);
 
-function changeNavBar(){
-    let header = document.querySelector("header");
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('header-scroll-active', windowPosition);
+function changeNavBar() {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("header-scroll-active", windowPosition);
+}
+
+function openMobMenu() {
+  mobMenu.classList.add("active");
+}
+
+function closeMobMenu() {
+  mobMenu.classList.remove("active");
 }
